2283b8eb463b64db1507bd3b8c36d4b23405291c
tatephughes <etatephughes@gmail.com>
2024-07-18 10:44:17 +0100

    Automatic Commit

--- run.sh
+++ run.sh
@@ -2,14 +2,21 @@
 
 read -p "Enter the file label (e.g. pc, laptop, test, hamilton): " label
 
+echo "Original C code output:"
+gcc ./src/main/adaptchol/adaptchol.c -o adaptchol -lm
+./src/main/adaptchol/adaptchol
+
+echo "Reading the generated target varience..."
+Rscript -e "source('./src/main/adaptchol/adaptb'); write.table(sig, file = './data/target_variance.csv', sep = ',', col.names = FALSE, row.names = FALSE)"
+
 echo "Scala Outputs (IC)"
 for ((i = 1; i < 6; i++)); do
-    sbt "runMain simple_run_IC ./data/scala_sample_basetest_IC_$label_$i" 
+    sbt "runMain simple_run_IC ./data/scala_sample_IC_$label_$i" 
 done
 
 echo "Scala Outputs (MD)"
 for ((i = 1; i < 6; i++)); do
-    sbt "runMain simple_run_MD ./data/scala_sample_basetest_MD_$label_$i" 
+    sbt "runMain simple_run_MD ./data/scala_sample_MD_$label_$i" 
 done
 
 
@@ -28,12 +35,12 @@ done
 
 echo "JAX Outputs (IC)"
 for ((i = 1; i < 6; i++)); do
-    sample_file="'./data/jax_sample_basetest_IC_${label}_${i}'"
+    sample_file="'./data/jax_sample_IC_${label}_${i}'"
     ~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; main(d=100, n=10000, thinrate=100, burnin=0, write_files=True, mix=False, sample_file=$sample_file, use_64=False)'
 done
 
 echo "JAX Outputs (MD)"
 for ((i = 1; i < 6; i++)); do
-    sample_file="'./data/jax_sample_basetest_MD_${label}_${i}'"
+    sample_file="'./data/jax_sample_MD_${label}_${i}'"
     ~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; main(d=100, n=10000, thinrate=100, burnin=0, write_files=True, mix=True, sample_file=$sample_file, use_64=False)'
 done
