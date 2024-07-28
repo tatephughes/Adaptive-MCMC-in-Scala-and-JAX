554de25887fbaf3bf1409178e089fa4d599ed580
tatephughes <etatephughes@gmail.com>
2024-07-28 17:23:40 +0000

    Re-re-re-did PC tests

--- run.sh
+++ run.sh
@@ -6,39 +6,39 @@ read -p "Enter the file label (e.g. pc, laptop, test, hamilton): " label
 #cd ./src/main/adaptchol/
 #gcc adaptchol.c -o adaptchol -lm
 #./adaptchol
-#
+
 #cd ../../../
 
-echo "Extracting the generated target varience..."
-Rscript -e "source('./src/main/adaptchol/adaptb'); write.table(sig, file = './data/target_variance.csv', sep = ',', col.names = FALSE, row.names = FALSE)"
-
-echo "Scala Outputs (IC)"
-for ((i = 1; i < 6; i++)); do
-    sample_file="./data/scala_sample_IC_${label}_${i}"
-    var_label="${label}_${i}" 
-    sbt "runMain simple_run_IC ${sample_file} ${var_label} 100 ./data/target_variance.csv" 
-done
-
-echo "Scala Outputs (MD)"
-for ((i = 1; i < 6; i++)); do
-    sample_file="./data/scala_sample_MD_${label}_${i}"
-    var_label="${label}_${i}" 
-    sbt "runMain simple_run_MD ${sample_file} ${var_label} 100 ./data/target_variance.csv" 
-done
+#echo "Extracting the generated target varience..."
+#Rscript -e "source('./src/main/adaptchol/adaptb'); write.table(sig, file = './data/target_variance.csv', sep = ',', col.names = FALSE, row.names = FALSE)"
+#
+#echo "Scala Outputs (IC)"
+#for ((i = 1; i < 6; i++)); do
+#    sample_file="./data/scala_sample_IC_${label}_${i}"
+#    var_label="${label}_${i}" 
+#    sbt "runMain simple_run_IC ${sample_file} ${var_label} 100 ./data/target_variance.csv" 
+#done
+#
+#echo "Scala Outputs (MD)"
+#for ((i = 1; i < 6; i++)); do
+#    sample_file="./data/scala_sample_MD_${label}_${i}"
+#    var_label="${label}_${i}" 
+#    sbt "runMain simple_run_MD ${sample_file} ${var_label} 100 ./data/target_variance.csv" 
+#done
 
 
 echo "R Outputs (IC)"
 for ((i = 1; i < 6; i++)); do
     sample_file="'./data/r_sample_IC_${label}_${i}'"
     var_label="'${label}_${i}'" 
-    Rscript -e "source('./src/main/R/AM_in_R.R'); sample <- main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files = TRUE, mix = FALSE, sample_file=${sample_file}, var_labels=${var_label})"
+    Rscript -e "source('./src/main/R/AM_in_R.R'); sample <- main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files = TRUE, mix = FALSE, sample_file=${sample_file}, var_labels=${var_label}, seed=${i})"
 done
 
 echo "R Outputs (MD)"
 for ((i = 1; i < 6; i++)); do
     sample_file="'./data/r_sample_MD_${label}_${i}'"
     var_label="'${label}_${i}'" 
-    Rscript -e "source('./src/main/R/AM_in_R.R'); sample <- main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files = TRUE, mix = TRUE, sample_file=${sample_file}, var_labels=${var_label})"
+    Rscript -e "source('./src/main/R/AM_in_R.R'); sample <- main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files = TRUE, mix = TRUE, sample_file=${sample_file}, var_labels=${var_label}, seed=${i})"
 done
 
 
@@ -53,5 +53,5 @@ echo "JAX Outputs (MD)"
 for ((i = 1; i < 6; i++)); do
     sample_file="'./data/jax_sample_MD_${label}_${i}'"
     var_label="'${label}_${i}'" 
-    ~/CPUJAX/bin/python -c "import sys; sys.path.append('./src/main/Python-JAX/'); from AM_in_JAX import *; main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files=True, mix=True, sample_file = ${sample_file}, use_64=False, var_labels=${var_label})"
+    ~/CPUJAX/bin/python -c "import sys; sys.path.append('./src/main/Python-JAX/'); from AM_in_JAX import *; main(sigma=read_sigma(100, './data/target_variance.csv'), n=10000, thinrate=100, burnin=0, write_files=True, mix=True, sample_file = ${sample_file}, use_64=False, var_labels=${var_label}, seed=${i})"
 done
--- run_complexity_tests.sh
+++ run_complexity_tests.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 echo "JAX output (IC, 32-bit): "
-~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=False, csv_file="./data/JAX_32_compute_times_laptop_1_IC.csv", is_64_bit=False)'
+~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=False, csv_file="./data/JAX_32_compute_times_pc_1_IC.csv", is_64_bit=False)'
 
 echo "JAX output (MD, 32-bit): "
-~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=True, csv_file="./data/JAX_32_compute_times_laptop_1_MD.csv", is_64_bit=False)'
+~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=True, csv_file="./data/JAX_32_compute_times_pc_1_MD.csv", is_64_bit=False)'
 
 
 echo "JAX output (IC, 64-bit): "
-~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=False, csv_file="./data/JAX_64_compute_times_laptop_1_IC.csv", is_64_bit=True)'
+~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=False, csv_file="./data/JAX_64_compute_times_pc_1_IC.csv", is_64_bit=True)'
 
 echo "JAX output (MD, 64-bit): "
-~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=True, csv_file="./data/JAX_64_compute_times_laptop_1_MD.csv", is_64_bit=True)'
+~/CPUJAX/bin/python -c 'import sys; sys.path.append("./src/main/Python-JAX/"); from AM_in_JAX import *; compute_time_graph(read_sigma(d=100), mix=True, csv_file="./data/JAX_64_compute_times_pc_1_MD.csv", is_64_bit=True)'
 
 
 echo "R output (IC): "
-Rscript -e 'source("./src/main/R/AM_in_R.R"); compute_time_graph(read_sigma(100), csv_file="./data/R_compute_times_laptop_1_IC.csv", mix=FALSE)'
+Rscript -e 'source("./src/main/R/AM_in_R.R"); compute_time_graph(read_sigma(100), csv_file="./data/R_compute_times_pc_1_IC.csv", mix=FALSE)'
 
 echo "R output (MD): "
-Rscript -e 'source("./src/main/R/AM_in_R.R"); compute_time_graph(read_sigma(100), csv_file="./data/R_compute_times_laptop_1_MD.csv", mix=TRUE)'
+Rscript -e 'source("./src/main/R/AM_in_R.R"); compute_time_graph(read_sigma(100), csv_file="./data/R_compute_times_pc_1_MD.csv", mix=TRUE)'
 
 
 echo "Scala output (IC): "
-sbt "runMain complexity_run_IC ./data/scala_compute_times_laptop_1_IC.csv 100"
+sbt "runMain complexity_run_IC ./data/scala_compute_times_pc_1_IC.csv 100"
 
 echo "Scala output (MD): "
-sbt "runMain complexity_run_MD ./data/scala_compute_times_laptop_1_MD.csv 100"
+sbt "runMain complexity_run_MD ./data/scala_compute_times_pc_1_MD.csv 100"
 
 #echo "Scala Java Blas"
 #sbt -Ddev.ludovic.netlib.blas.nativeLib=libopenblasobutnot "runMain complexity_run_IC ./data/scala_compute_times_laptop_javablas.csv 50"
